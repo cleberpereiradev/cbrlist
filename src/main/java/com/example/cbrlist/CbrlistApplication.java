95c3b856d160aaab2331adf6e68fe73a0b05d00f
cleberpereiradev <cleberpereiraeng@gmail.com>
2023-07-10 21:25:28 -0300

    Project created

---
+++ src/main/java/com/example/cbrlist/CbrlistApplication.java
@@ -0,0 +1,13 @@
+package com.example.cbrlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbrlistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CbrlistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cbrlist/CbrlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cbrlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbrlistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
