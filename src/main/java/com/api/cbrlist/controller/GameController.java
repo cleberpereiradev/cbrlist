9cc10514d3b90d99bc393b87c987edc5bd4a1c93
cleberpereiradev <cleberpereiraeng@gmail.com>
2023-07-11 21:21:31 -0300

    Seed update and model maps

--- src/main/java/com/api/cbrlist/controller/GameController.java
+++ src/main/java/com/api/cbrlist/controller/GameController.java
@@ -1,9 +1,12 @@
 package com.api.cbrlist.controller;
 
+import com.api.cbrlist.dto.GameDTO;
 import com.api.cbrlist.dto.GameMinDTO;
+import com.api.cbrlist.entities.Game;
 import com.api.cbrlist.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +24,12 @@ public class GameController {
         List<GameMinDTO> games = service.findAll();
         return games;
     }
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        GameDTO result = service.findById(id);
+        return result;
+    }
+
+
 }
--- src/main/java/com/api/cbrlist/service/GameService.java
+++ src/main/java/com/api/cbrlist/service/GameService.java
@@ -1,10 +1,12 @@
 package com.api.cbrlist.service;
 
+import com.api.cbrlist.dto.GameDTO;
 import com.api.cbrlist.dto.GameMinDTO;
 import com.api.cbrlist.entities.Game;
 import com.api.cbrlist.repository.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -18,4 +20,10 @@ public class GameService {
         List<Game> games = repo.findAll();
         return games.stream().map(GameMinDTO::new).toList();
     }
+
+    public GameDTO findById(@PathVariable Long id) {
+        Game result = repo.findById(id).get();
+        return new GameDTO(result);
+    }
+
 }
